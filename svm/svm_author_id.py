bbd5546738ae81e17ea10c870d71668b921b14b7
Oleg <xdiver077@gmail.com>
2017-01-12 19:42:23 -0800

    Done Lesson 2: Support Vector Machines (SVM) project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,44 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+
+#clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000.0)
+
+
+# Reduce training set
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+# Training of the Model
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+# Test prediction
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+###=============TASK
+print "Prediction 10", pred[10]
+print "Prediction 26", pred[26]
+print "Prediction 50", pred[50]
+
+j = 0
+for i in range(len(pred)):
+    if pred[i] == 1:
+        j += 1
+print "Chris (1) class has ", j
+#===================
+
+### Calculate an accuracy
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+
+
+print "Accuracy = ", accuracy
 
 #########################################################
 
